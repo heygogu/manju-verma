2745660057254cce2bd9a625211c2bdf96697ced
heygogu <rohitnarnolia88@gmail.com>
2025-03-19 13:06:09 +0530

    remove sitemap location

--- app/robots.txt/route.ts
+++ app/robots.txt/route.ts
@@ -4,7 +4,7 @@ export async function GET() {
   return new NextResponse(
     `User-agent: *
     Allow: /
-    Sitemap: https://manjuverma.com/api/sitemap.xml`,
+    Sitemap: https://manjuverma.com/sitemap.xml`,
     { headers: { "Content-Type": "text/plain" } }
   );
 }
